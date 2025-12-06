88862cb3ee8c2f669bbb629d822bff156556a3b1
Fernando <saudor373@gmail.com>
2025-12-05 20:56:42 -0600

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  productionBrowserSourceMaps: false,
+  turbopack: {},
+}
+
+export default nextConfig
